12fc3bff94873104e7fb3654b295dde2f5926423
yowaimono <zhangyanhai278@gmail.com>
2024-10-18 12:26:56 +0800

    增加GetBase64,GetImage等方法

--- captcha.go
+++ captcha.go
@@ -4,13 +4,21 @@ package captcha
 import (
 	"bytes"
 	"encoding/base64"
+	"image"
 	"image/png"
 	"math/rand"
+	"os"
 	"time"
 )
 
 // GetOne 生成一张验证码图片，并返回验证码ID和base64编码的图片
 func GetOne(length int, format CaptchaFormat) (string, string, error) {
+	// 直接调用GetBase64方法
+	return GetBase64(length, format)
+}
+
+// GetBase64 生成一张验证码图片，并返回验证码ID和base64编码的图片
+func GetBase64(length int, format CaptchaFormat) (string, string, error) {
 	// 生成指定长度和格式的随机验证码
 	code := generateCaptchaCode(length, format)
 
@@ -40,6 +48,64 @@ func GetOne(length int, format CaptchaFormat) (string, string, error) {
 	}()
 
 	return captchaID, imgBase64, nil
+}
+
+// GetImage 生成一张验证码图片，并返回验证码ID和image.Image对象
+func GetImage(length int, format CaptchaFormat) (string, image.Image, error) {
+	// 生成指定长度和格式的随机验证码
+	code := generateCaptchaCode(length, format)
+
+	// 创建验证码图片
+	img := createCaptchaImage(code)
+
+	// 生成验证码ID
+	captchaID := generateCaptchaID()
+
+	// 存储验证码信息
+	storeCaptcha(captchaID, code)
+
+	// 启动一个goroutine来删除过期的验证码
+	go func() {
+		time.Sleep(60 * time.Second)
+		deleteCaptcha(captchaID)
+	}()
+
+	return captchaID, img, nil
+}
+
+// GetAndSave 生成一张验证码图片，并将其保存到指定路径，返回验证码ID和验证码内容
+func GetAndSave(length int, format CaptchaFormat, savePath string) (string, string, error) {
+	// 生成指定长度和格式的随机验证码
+	code := generateCaptchaCode(length, format)
+
+	// 创建验证码图片
+	img := createCaptchaImage(code)
+
+	// 将图片保存到指定路径
+	file, err := os.Create(savePath)
+	if err != nil {
+		return "", "", err
+	}
+	defer file.Close()
+
+	err = png.Encode(file, img)
+	if err != nil {
+		return "", "", err
+	}
+
+	// 生成验证码ID
+	captchaID := generateCaptchaID()
+
+	// 存储验证码信息
+	storeCaptcha(captchaID, code)
+
+	// 启动一个goroutine来删除过期的验证码
+	go func() {
+		time.Sleep(60 * time.Second)
+		deleteCaptcha(captchaID)
+	}()
+
+	return captchaID, code, nil
 }
 
 // 生成验证码ID
